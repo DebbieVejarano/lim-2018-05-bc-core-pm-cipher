14ca3bcfa716b67c3535b1ffa1f5d50ccc28952c
DebbieVejarano <alexandra.vejarano@gmail.com>
2018-05-30 00:06:06 -0500

    Lógica de programación finalizada

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,33 @@
-window.cipher = {
-  // ... 
+/* const codificar = function codificar (offset , firstText) {
+  let n = offset;
+  let finalText = '';
+
+  for (let i=0; i < firstText.length; i++){
+    //debugger
+    const x = firstText.charCodeAt(i);
+    
+    if (64<x && x<91){
+      let cipher = (x - 65 + n)%26 + 65;
+      finalText += String.fromCharCode(cipher);
+    } else if (96<x && x<123) {
+        let cipher = (x - 97 + n)%26 + 97;
+        finalText += String.fromCharCode(cipher);
+    } else {
+      finalText += String.fromCharCode(x);
+    }  
+ }
+ return finalText
 };
+
+const decodificar = function decodificar (offset, firstText) {
+  let n = offset;
+  let y = string;
+
+
+}; 
+
+window.cipher = {
+  encode: codificar,
+  decode: decodificar
+};
+ */
--- src/index.js
+++ src/index.js
@@ -4,14 +4,91 @@ const secondText = document.getElementById("secondText");
 const cipherButton = document.getElementById("cipherButton");
 const decipherButton = document.getElementById("decipherButton");
 const cleanButton = document.getElementById("clean");
-const letterJumpUser = document.getElementById("letterJumpUser");
-let letterJump = 0; //Lo creamos para llamarlo en la función CaesarCipher y obtener el VALUE
-let finalText;
+let letterJump = 0;
 
-firstText.addEventListener ("input", () => {
+
+
+//BOTÓN CIFRAR
+cipherButton.addEventListener("click", () => {
+    letterJump = parseInt(document.getElementById("letterJump").value);
+    //console.log(letterJump);
+    secondText.value = codificar (firstText.value, letterJump);
+})
+
+//BOTÓN DESCIFRAR
+decipherButton.addEventListener("click", () => {
+    letterJump = parseInt(document.getElementById("letterJump").value);
+    secondText.value = decodificar (firstText.value, letterJump);
+})
+
+
+//Función cifrar
+
+function codificar (message ,offset) {
+    let n = offset;
+    let finalText = '';
+  
+    for (let i=0; i < message.length; i++){
+      //debugger
+      const x = message.charCodeAt(i);
+      
+      if (64<x && x<91){
+        let cipher = (x - 65 + n)%26 + 65;
+        finalText += String.fromCharCode(cipher);
+      } else if (96<x && x<123) {
+          let cipher = (x - 97 + n)%26 + 97;
+          finalText += String.fromCharCode(cipher);
+      } else {
+        finalText += String.fromCharCode(x);
+      }  
+   }
+   return finalText;
+  };
+
+  //función descifrar
+
+function decodificar (message, offset) {
+    let n = offset;
+    let finalText = '';
+
+    for (let i=0; i < message.length; i++) {
+        const y = message.charCodeAt(i);
+           
+        if (64<y && y<91){
+            let result = (y - 65 - n)%26;
+               
+               if ( result >= 0) {
+               let decipher = result + 65;
+               finalText += String.fromCharCode(decipher);
+               } else {
+               let decipher = (result + 65) + 26;
+               finalText += String.fromCharCode(decipher);
+               }
+        } else if ((96<y && y<123)){
+               let result = (y - 97 - n)%26;
+               
+               if ( result >= 0) {
+               let decipher = result + 97;
+               finalText += String.fromCharCode(decipher);
+               } else {
+               let decipher = (result + 97) + 26;
+               finalText += String.fromCharCode(decipher);
+               }
+        } else {
+               finalText += String.fromCharCode(y);
+           }
+       }
+    return finalText;
+  }; 
+/*
+offsetPast.addEventListener("input", () => {
+    console.log(offset.value);
+})
+//offset;
+ firstText.addEventListener ("input", () => {
     return firstText.value;
 
-})
+}) 
 
 //botón cifrar
 cipherButton.addEventListener("click", () => {
@@ -46,4 +123,59 @@ function caesarCipher(firstText, letterJump) {
 		finalText += String.fromCharCode(firstText.charCodeAt(i)+letterJump);
     }
     return finalText;
-}
+} */ 
+
+//CIFRAR
+/* const message = prompt('Ingresa un mensaje');
+let n = 3;
+
+for (let i=0; i < message.length; i++){
+    //debugger
+    const x = message.charCodeAt(i);
+    
+    if (64<x && x<91){
+        //console.log(x);
+      let cipher = (x - 65 + n)%26 + 65;
+    console.log(String.fromCharCode(cipher));
+    } else if (96<x && x<123) {
+        let cipher = (x - 97 + n)%26 + 97;
+        console.log(String.fromCharCode(cipher));
+    } else {
+        console.log(String.fromCharCode(x));
+    }
+
+    
+ } */
+
+ //DESCIFRAR
+/*  const message = prompt('Escribe tu mensaje');
+let n = 3;
+//Descifrando
+for (let i=0; i < message.length; i++) {
+ //debugger
+    const y = message.charCodeAt(i);
+    
+    if (64<y && y<91){
+        let result = (y - 65 - n)%26;
+        
+        if ( result >= 0) {
+        let decipher = result + 65;
+        console.log(String.fromCharCode(decipher));
+        } else {
+        let decipher = (result + 65) + 26;
+        console.log(String.fromCharCode(decipher));
+        }
+} else if ((96<y && y<123)){
+        let result = (y - 97 - n)%26;
+        
+        if ( result >= 0) {
+        let decipher = result + 97;
+        console.log(String.fromCharCode(decipher));
+        } else {
+        let decipher = (result + 97) + 26;
+        console.log(String.fromCharCode(decipher));
+        }
+} else {
+        console.log(String.fromCharCode(y));
+    }
+} */
